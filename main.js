c0ecdd68b5fb40417ec163282757ae2d988e49eb
Phong Truong <phongoli0@gmail.com>
2019-07-15 16:34:49 -0700

    added modal launch modal on win

--- main.js
+++ main.js
@@ -25,6 +25,7 @@ function initializeApp() {
   randomizeAndGenerateCards();
   $(".card").click(cardClicked);
   $(".resetButton").click(resetGame);
+  winModal();
 }
 
 
@@ -80,10 +81,10 @@ function cardClicked() {
     return;
   }
   if (first_card_clicked === null) {
-    open();
     first_card_clicked = $(this);
     first_card_clicked.parent().addClass("click");
     pickSound();
+    openModal();
     return;
   } else {
     second_card_clicked = $(this);
@@ -98,7 +99,7 @@ function cardClicked() {
       matchSound();
       accuracy = match_counter / attempts;
       if (match_counter === total_possible_matches) {
-        alert("you won");
+        openModal();
       }
       first_card_clicked = null;
       second_card_clicked = null;
@@ -143,5 +144,36 @@ function resetGame() {
   shuffle(images);
   randomizeAndGenerateCards();
   $(".card").click(cardClicked);
+}
+
+function winModal(){
+    // Get the modal
+    var modal = document.getElementById("myModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("myBtn");
+  
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+  
+    // When the user clicks on the button, open the modal 
+    btn.onclick = function() {
+      modal.style.display = "block";
+  }
+  
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+  }
+  
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+    }
+  }
+    openModal = function() {
+      modal.style.display = "block";
+  }
 }
 
